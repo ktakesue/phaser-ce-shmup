b4cbe269e79e23e2dffd56a736b908c5c3c3e5b6
ktakesue <ktakesue@aol.com>
2018-01-25 18:52:36 -1000

    phaser is running

--- js/game.js
+++ js/game.js
@@ -1,3 +1,28 @@
 (Phaser => {
-  console.log(Phaser);
+  const GAME_WIDTH = 460;
+  const GAME_HEIGHT = 600;
+  const GAME_CONTAINER_ID = 'game';
+
+  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, {preload, create, update});
+  // {
+  //   preload : preload,
+  //   create : create,
+  //   update : update,
+  // }
+  
+  // core game methods //  
+  function preload() {
+
+  }
+
+  function create() {
+
+  }
+
+  function update(){
+
+  }
+
+
+  // console.log(Phaser);
 })(window.Phaser);
